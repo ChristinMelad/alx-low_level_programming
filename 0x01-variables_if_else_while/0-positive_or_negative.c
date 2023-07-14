55e89e1784164f0ff6198709446724811c89e92f
root <root@Christin>
2023-07-14 16:34:16 +0300

    if last didgit statment

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
  *main - entry point
  *
  *Description: positive or negative
  *
  *Return: 0 (success)
-*/
+ */
 
 int main(void)
 {
@@ -18,10 +17,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	else
-	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
