672a406829df7d46b948f8f0cb3dcbec40f0a5c8
root <root@Christin>
2023-07-18 21:50:02 +0300

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,19 +3,17 @@
 /**
 *main -entry point.
 *
-*print_alphabet - make the alphabet a - z
+*print_alphabet_x10 - function will print the alphabet 10 times
 *
-*Return: void
 */
-
-void print_alphabet(void);
-
+void print_alphabet_x10(void);
 {
-char c;
+int line, c;
 
+for (line = 0; line <= 9; line++)
+{
 for (c = 'a'; c <= 'z'; c++)
-{
 _putchar(c);
-}
 _putchar('\n');
+}
 }
