eb2887942e757d321efc3cb451c970feb630c99b
root <root@Christin>
2023-07-18 21:28:07 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*main -entry point.
+*
+*print_alphabet - make the alphabet a - z
+*
+*Return: void
+*/
+
+void print_alphabet(void);
+
+{
+
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
