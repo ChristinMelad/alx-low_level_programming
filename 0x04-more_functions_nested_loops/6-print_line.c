80344f38b862761da1a669954f4c53f164eac9b8
root <root@Christin>
2023-07-20 14:52:59 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_line - prints a straight line
+*@n:is the number of times the _ should be printed
+*/
+
+void print_line(int n)
+{
+int lnchr;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (lnchr = 1; lnchr <= n; lnchr++)
+_putchar('95');
+_putchar('\n');
+}
+}
