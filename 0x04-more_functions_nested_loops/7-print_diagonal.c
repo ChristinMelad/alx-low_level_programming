46ffe3ee250781c5bfab6f1b933bc1cf8aa7502e
root <root@Christin>
2023-07-20 15:27:03 +0300

    print digonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,8 @@
 /**
 *print_diagonal - prints a diagonal line
 *
-*@n:is the number of times the _ should be printed
+*@n:is the number of times the \ character
+*       should be printed
 */
 
 void print_diagonal(int n)
