3590b5419ff0d89b97b74a37e7d145adcccfe9a2
root <root@Christin>
2023-08-03 02:15:35 +0300

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,33 +2,25 @@
 
 /**
 * _sqrt_recursion - returns the natural square root of a number
+* _sqrt_helper - helps to check if i * i equal n
 *@n: number to calulate
 *Return: natural square root or -1 if n doen't have natural sqr
 */
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-		if (n == 0 || n == 1)
-		return (n);
-		return (_sqrt_helper(n, 1));
+if (n < 0)
+return (-1);
+if (n == 0 || n == 1)
+return (n);
+return (_sqrt_helper(n, 1));
 }
 
-/**
-*_sqrt_helper - check if i * i is equal n
-*@n: number to calulate
-*@i: the current guess for the square root of n
-*Return: natural square root or -1 if n doen't have natural sqr
-*/
-
-
 int _sqrt_helper(int n, int i)
-
 {
-	if (i * i == n)
-	return (i);
-	if (i * i > n)
-	return (-1);
-	return (_sqrt_helper(n, i + 1));
-	}
+if (i * i == n)
+return (i);
+if (i * i > n)
+return (-1);
+return (_sqrt_helper(n, i + 1));
+}
