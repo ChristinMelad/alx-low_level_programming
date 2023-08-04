8c40dee85c606a000a8741d308e905027efced11
root <root@Christin>
2023-08-04 17:30:14 +0300

    argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints result of the multiplication, followed by a new line
+*@argc: number of arguments
+*@argv: arguments
+*Return: 0 success, else error and return 1
+*/
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+if (argc == 3)
+{
+result = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", result);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
