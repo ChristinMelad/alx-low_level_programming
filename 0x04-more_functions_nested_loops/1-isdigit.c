246e410f158c4ec5d92e32ea11a2c96f25320106
root <root@Christin>
2023-08-04 12:11:53 +0300

    updates

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
-* _isdigit - chceck if input is digit number between 0-9
+* _isdigit - check if input is digit number between 0-9.
 *
-*@c: input
+*@c: char to check
 *
-*Return: 1 if it's dgit, 0 if not
+*Return: 1 if it's digit, 0 if not.
 */
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
+
+	if (c >= '0' && c <= '9')
+	{
 	return (1);
-	else
-	return (0);
+	}
+else
+{
+return (0);
+}
 }
