9a9c06bd83b1eb8d39eefe4b66439f58e2de6746
root <root@Christin>
2023-08-04 22:31:53 +0300

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,7 +6,7 @@
 *main - Entry point
 *@argc: count number of argumetns
 *@argv: arguments
-*Return: always 0
+*Return: 0 if no number is passed to the program, if no digit print Error
 */
 
 int main(int argc, char **argv)
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 int sum = 0;
 int i;
 
-for (i = 0; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
 int num = atoi(argv[i]);
 if (num <= 0)
