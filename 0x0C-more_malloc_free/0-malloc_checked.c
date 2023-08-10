2a607610ca9346280fc672d3cd82a6a03731f218
root <root@Christin>
2023-08-10 20:52:17 +0300

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+*malloc_checked -check the allocated memory
+*@b: unsigned int
+*Return: return pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+
+void *ptr = malloc(sizeof(b));
+
+if (ptr == NULL)
+{
+	printf("malloc failed to allocate memory\n");
+	exit(0);
+}
+return (ptr);
+}
