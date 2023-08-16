8c9a186d3db8a65e229f6c0e877ced816cbde1a5
root <root@Christin>
2023-08-16 10:59:42 +0300

    pointers to functions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,11 +3,13 @@
 
 
 /**
-*
-*
-*
-*
+*array_iterator - prints an integer
+*@array: pointer to an int
+*@action: pointer to the function
+*@size: size of array
+*Return : nothing
 */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 
