b300fe41e82a8e66551f943f986072660157f921
root <root@Christin>
2023-08-16 23:39:33 +0300

    pointers to functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,33 +12,30 @@
 
 int main(int argc, char *argv[])
 {
-int a, b;
-char *op;
+char op;
+int (*op_func)(int, int);
+int result;
 
 if (argc != 4)
 {
-	printf("Error\n");
-	exit(98);
+printf("Error\n");
+return (98);
 }
-
-a = atoi(argv[1]);
-op = argv[2];
-b = atoi(argv[3]);
-
-if (get_op_func(op) == NULL || op[10] != '\0')
-
+op = *(argv[2]);
+if ((op == '/' || op == '%') && !atoi(argv[3]))
 {
-	printf("Error\n");
-	exit(99);
+printf("Error\n");
+return (100);
 }
 
-if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
 
+op_func = get_op_func(&op);
+if (op_func == NULL || argv[2][1] != '\0')
 {
-	printf("Error\n");
-	exit(100);
+printf("Error\n");
+return (99);
 }
-
-printf("%d\n", get_op_func(op)(a, b));
+result = op_func(atoi(argv[1]), atoi(argv[3]));
+printf("%d\n", result);
 return (0);
 }
