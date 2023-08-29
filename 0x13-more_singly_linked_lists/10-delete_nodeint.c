848dfb887c09f8a0e2ef7dd06f66af57b1b5c42c
ChristinMelad <christinmelad@yahoo.com>
2023-08-29 22:44:36 +0300

    more linked lists

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+*delete_nodeint_at_index - deletes node
+*@head: pointer to the forst node
+*@index: index of node
+*Return: 1 on success, -1 on fails
+*/
+
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node, *prev_node;
+
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (-1);
+
+	if (index == 0)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+node = *head;
+
+	while (node)
+	{
+		if (i == index)
+		{
+			prev_node->next = node->next;
+			free(node);
+			return (1);
+		}
+		i++;
+		prev_node = node;
+		node = node->next;
+	}
+	return (-1);
+}
